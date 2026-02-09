2ffabbcb72a05f167c19bbcfa16824326728f0bd
Randall Frank <frogboots.000@gmail.com>
2026-02-09 11:25:22 -0500

    Add some animation to the title
    - version 1.3.2
    - title screen is animated
    - high scores are displayed before play

--- build.py
+++ build.py
@@ -77,7 +77,7 @@ if not prerequisites:
 
 # Set the version number and start the build process
 # Must be 5 characters
-version = [1,3,1]
+version = [1,3,2]
 s=""
 for v in version:
     s += f"{int(v):02x}0a"
